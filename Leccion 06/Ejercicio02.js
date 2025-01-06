1fdacc6e28320e098545534b69072fbb973d3f97
Jose Galvez <jlgalvez77@gmail.com>
2025-01-06 20:51:21 +0100

    Lecciion06

---
+++ Leccion 06/Ejercicio02.js
@@ -0,0 +1,10 @@
+// Realizar la suma de los primeros 10 números naturales.
+// Utilizar un ciclo for.
+
+let suma = 0;
+
+for (let i = 1; i <= 10; i++){
+    suma += i;
+}
+
+console.log(suma);
---
+++ Leccion 06/while.js
@@ -0,0 +1,6 @@
+n = 0
+
+while(n < 5){
+    console.log(n);
+    n++;
+}
